f11db140ed2a76d68ec15ddfd8118c7fe7896670
lady ada <limor@ladyada.net>
2020-09-02 13:23:05 -0400

    basics working!

--- adafruit_lc709203f.py
+++ adafruit_lc709203f.py
@@ -36,6 +36,10 @@ __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_LC709203F.git"
 LC709023F_I2CADDR_DEFAULT = const(0x0B)
 LC709203F_CMD_ICVERSION = const(0x11)
 LC709203F_CMD_POWERMODE = const(0x15)
+LC709203F_CMD_APA = const(0x0B)
+LC709203F_CMD_INITRSOC = const(0x07)
+LC709203F_CMD_CELLVOLTAGE = const(0x09)
+LC709203F_CMD_CELLITE = const(0x0F)
 
 class CV:
     """struct helper"""
@@ -59,7 +63,6 @@ class CV:
 
 class PowerMode(CV):
     """Options for ``power_mode``"""
-
     pass  # pylint: disable=unnecessary-pass
 
 
@@ -71,6 +74,22 @@ PowerMode.add_values(
 )
 
 
+class PackSize(CV):
+    """Options for ``pack_size``"""
+    pass  # pylint: disable=unnecessary-pass
+
+
+PackSize.add_values(
+    (
+        ("MAH100", 0x08, "100 mAh", 100),
+        ("MAH200", 0x0B, "200 mAh", 200),
+        ("MAH500", 0x10, "500 mAh", 500),
+        ("MAH1000", 0x19, "1000 mAh", 1000),
+        ("MAH2000", 0x2D, "2000 mAh", 2000),
+        ("MAH3000", 0x36, "3000 mAh", 3000),
+    )
+)
+
 
 
 class LC709023F:
@@ -80,6 +99,64 @@ class LC709023F:
         self.i2c_device = i2c_device.I2CDevice(i2c_bus, address)
         self._buf = bytearray(10)
         self.power_mode = PowerMode.OPERATE
+        self.pack_size = PackSize.MAH500
+        self.init_RSOC()
+        
+    def init_RSOC(self):
+        self._write_word(LC709203F_CMD_INITRSOC, 0xAA55);
+
+    @property
+    def cell_voltage(self):
+        """Returns floating point voltage"""
+        return self._read_word(LC709203F_CMD_CELLVOLTAGE) / 1000
+
+    @property
+    def cell_percent(self):
+        """Returns percentage of cell capacity"""
+        return self._read_word(LC709203F_CMD_CELLITE) / 10
+
+    @property
+    def ic_version(self):
+        """Returns read-only chip version"""
+        return self._read_word(LC709203F_CMD_ICVERSION)
+    
+
+    @property
+    def power_mode(self):
+        """Returns current power mode (operating or sleeping)"""
+        return self._read_word(LC709203F_CMD_POWERMODE)
+    
+    @power_mode.setter
+    def power_mode(self, mode):
+        if not PowerMode.is_valid(mode):
+            raise AttributeError("power_mode must be a PowerMode")
+        self._write_word(LC709203F_CMD_POWERMODE, mode)
+
+    @property
+    def pack_size(self):
+        """Returns current battery pack size"""
+        return self._read_word(LC709203F_CMD_APA)
+    
+    @pack_size.setter
+    def pack_size(self, size):
+        if not PackSize.is_valid(size):
+            raise AttributeError("pack_size must be a PackSize")
+        self._write_word(LC709203F_CMD_APA, size)
+
+    # pylint: disable=no-self-use
+    def _generate_crc(self, data):
+        """8-bit CRC algorithm for checking data"""
+        crc = 0x00
+        # calculates 8-Bit checksum with given polynomial
+        for byte in data:
+            crc ^= byte
+            for _ in range(8):
+                if crc & 0x80:
+                    crc = (crc << 1) ^ 0x07
+                else:
+                    crc <<= 1
+        return crc & 0xFF
+
 
     def _read_word(self, command):
         self._buf[0] = LC709023F_I2CADDR_DEFAULT * 2 # write byte
@@ -103,35 +180,3 @@ class LC709023F:
 
         with self.i2c_device as i2c:
             i2c.write(self._buf[1:5])
-        
-    @property
-    def ic_version(self):
-        """Returns read-only chip version"""
-        return self._read_word(LC709203F_CMD_ICVERSION)
-    
-
-    @property
-    def power_mode(self):
-        """Returns current power mode (operating or sleeping)"""
-        return self._read_word(LC709203F_CMD_POWERMODE)
-    
-    @power_mode.setter
-    def power_mode(self, mode):
-        if not PowerMode.is_valid(mode):
-            raise AttributeError("power_mode must be a PowerMode")
-        self._write_word(LC709203F_CMD_POWERMODE, mode)
-
-
-    # pylint: disable=no-self-use
-    def _generate_crc(self, data):
-        """8-bit CRC algorithm for checking data"""
-        crc = 0x00
-        # calculates 8-Bit checksum with given polynomial
-        for byte in data:
-            crc ^= byte
-            for _ in range(8):
-                if crc & 0x80:
-                    crc = (crc << 1) ^ 0x07
-                else:
-                    crc <<= 1
-        return crc & 0xFF
